9e2688c07f67c7b3ca2e6e12af305f7804073c28
HS <141958327+thepeacefuldude@users.noreply.github.com>
2026-08-24 06:43:13 +0800

    Fixed mob death animation

--- Assets/Scripts/StateMachineBehaviour/MobDeath.cs
+++ Assets/Scripts/StateMachineBehaviour/MobDeath.cs
@@ -4,8 +4,6 @@ public class MobDeath : StateMachineBehaviour
 {
     [SerializeField] private EnemySystem enemySystem;
     [SerializeField] private CharacterStats characterStats;
-    [SerializeField] private GameObject expOrbPrefab;
-    [SerializeField] private int expReward = 10;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -24,13 +22,6 @@ public class MobDeath : StateMachineBehaviour
     {
         animator.gameObject.SetActive(false);
         enemySystem.enemyLeft -= 1;
-
-        // Spawn exp orb on death
-        if (expOrbPrefab != null)
-        {
-            GameObject orb = Instantiate(expOrbPrefab, animator.transform.position, Quaternion.identity);
-            orb.GetComponent<ExpOrb>().SetExp(expReward);
-        }
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
